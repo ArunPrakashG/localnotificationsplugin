853b87ddca12c0a91233a7c5cb3946159e206ef9
Arun Prakash <arun.prakash.456789@gmail.com>
2021-01-09 17:10:37 +0530

    Scheduled notifications

--- src/Plugin.LocalNotifications.Android/LocalNotificationsImplementation.cs
+++ src/Plugin.LocalNotifications.Android/LocalNotificationsImplementation.cs
@@ -1,157 +1,254 @@
+using Android;
 using Android.App;
+using Android.App.Job;
 using Android.Content;
+using Android.OS;
 using Android.Support.V4.App;
 using Plugin.LocalNotifications.Abstractions;
 using System;
 using System.IO;
 using System.Xml.Serialization;
-using Android.OS;
 
 namespace Plugin.LocalNotifications
 {
-    /// <summary>
-    /// Local Notifications implementation for Android
-    /// </summary>
-    public class LocalNotificationsImplementation : ILocalNotifications
-    {
-        string _packageName => Application.Context.PackageName;
-        NotificationManager _manager => (NotificationManager)Application.Context.GetSystemService(Context.NotificationService);
+	/// <summary>
+	/// Local Notifications implementation for Android
+	/// </summary>
+	public class LocalNotificationsImplementation : ILocalNotifications
+	{
+		string _packageName => Application.Context.PackageName;
 
-        /// <summary>
-        /// Get or Set Resource Icon to display
-        /// </summary>
-        public static int NotificationIconId { get; set; }
+		NotificationManager _manager => (NotificationManager)Application.Context.GetSystemService(Context.NotificationService);
 
-        /// <summary>
-        /// Show a local notification
-        /// </summary>
-        /// <param name="title">Title of the notification</param>
-        /// <param name="body">Body or description of the notification</param>
-        /// <param name="id">Id of the notification</param>
-        public void Show(string title, string body, int id = 0)
-        {
-            var builder = new Notification.Builder(Application.Context);
-            builder.SetContentTitle(title);
-            builder.SetContentText(body);
-            builder.SetAutoCancel(true);
+		/// <summary>
+		/// Get or Set Resource Icon to display
+		/// </summary>
+		public static int NotificationIconId { get; set; }
 
-            if (NotificationIconId != 0)
-            {
-                builder.SetSmallIcon(NotificationIconId);
-            }
-            else
-            {
-                builder.SetSmallIcon(Resource.Drawable.plugin_lc_smallicon);
-            }
+		private string CreateNotificationChannel(string channelId)
+		{
+			if (string.IsNullOrEmpty(channelId))
+			{
+				return "";
+			}
 
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                var channelId = $"{_packageName}.general";
-                var channel = new NotificationChannel(channelId, "General", NotificationImportance.Default);
+			if (DoesChannelExist(channelId))
+			{
+				return "";
+			}
 
-                _manager.CreateNotificationChannel(channel);
+			var channel = new NotificationChannel(channelId, "General", NotificationImportance.High);
+			channel.EnableVibration(true);
+			channel.LockscreenVisibility = NotificationVisibility.Public;
+			channel.SetBypassDnd(false);
+			channel.SetShowBadge(true);
+			channel.SetVibrationPattern(new long[] { 0, 250, 250, 250 });
+			_manager.CreateNotificationChannel(channel);
+			return channelId;
+		}
 
-                builder.SetChannelId(channelId);
-            }
+		private bool DoesChannelExist(string channelId) => !string.IsNullOrEmpty(channelId) && _manager.GetNotificationChannel(channelId) != null;
 
-            var resultIntent = GetLauncherActivity();
-            resultIntent.SetFlags(ActivityFlags.NewTask | ActivityFlags.ClearTask);
-            var stackBuilder = Android.Support.V4.App.TaskStackBuilder.Create(Application.Context);
-            stackBuilder.AddNextIntent(resultIntent);
-            var resultPendingIntent =
-                stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
-            builder.SetContentIntent(resultPendingIntent);
+		private bool IsChannelDisabled(string channelId)
+		{
+			if (string.IsNullOrEmpty(channelId))
+			{
+				return true;
+			}
 
-            _manager.Notify(id, builder.Build());
-        }
+			var channel = _manager.GetNotificationChannel(channelId);
 
-        public static Intent GetLauncherActivity()
-        {
-            var packageName = Application.Context.PackageName;
-            return Application.Context.PackageManager.GetLaunchIntentForPackage(packageName);
-        }
+			if (channel == null)
+			{
+				return false;
+			}
 
-        /// <summary>
-        /// Show a local notification at a specified time
-        /// </summary>
-        /// <param name="title">Title of the notification</param>
-        /// <param name="body">Body or description of the notification</param>
-        /// <param name="id">Id of the notification</param>
-        /// <param name="notifyTime">Time to show notification</param>
-        public void Show(string title, string body, int id, DateTime notifyTime)
-        {
-            var intent = CreateIntent(id);
+			return channel.Importance == NotificationImportance.None || channel.Importance == NotificationImportance.Unspecified;
+		}
 
-            var localNotification = new LocalNotification();
-            localNotification.Title = title;
-            localNotification.Body = body;
-            localNotification.Id = id;
-            localNotification.NotifyTime = notifyTime;
-            if (NotificationIconId != 0)
-            {
-                localNotification.IconId = NotificationIconId;
-            }
-            else
-            {
-                localNotification.IconId = Resource.Drawable.plugin_lc_smallicon;
-            }
+		/// <summary>
+		/// Show a local notification
+		/// </summary>
+		/// <param name="title">Title of the notification</param>
+		/// <param name="body">Body or description of the notification</param>
+		/// <param name="id">Id of the notification</param>
+		public void Show(string title, string body, int id = 0)
+		{
+			var builder = new Notification.Builder(Application.Context, CreateNotificationChannel($"{_packageName}.urgent"));
+			builder.SetContentTitle(title);
+			builder.SetContentText(body);
+			builder.SetAutoCancel(true);
 
-            var serializedNotification = SerializeNotification(localNotification);
-            intent.PutExtra(ScheduledAlarmHandler.LocalNotificationKey, serializedNotification);
+			if (NotificationIconId != 0)
+			{
+				builder.SetSmallIcon(NotificationIconId);
+			}
+			else
+			{
+				builder.SetSmallIcon(Resource.Drawable.plugin_lc_smallicon);
+			}
 
-            var pendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, intent, PendingIntentFlags.CancelCurrent);
-            var triggerTime = NotifyTimeInMilliseconds(localNotification.NotifyTime);
-            var alarmManager = GetAlarmManager();
+			//if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+			//{
+			//	var channelId = $"{_packageName}.general";
+			//	builder.SetChannelId(CreateNotificationChannel(channelId));
+			//}
 
-            alarmManager.Set(AlarmType.RtcWakeup, triggerTime, pendingIntent);
-        }
+			var resultIntent = GetLauncherActivity();
+			resultIntent.SetFlags(ActivityFlags.NewTask | ActivityFlags.ClearTask);
+			var stackBuilder = Android.Support.V4.App.TaskStackBuilder.Create(Application.Context);
+			stackBuilder.AddNextIntent(resultIntent);
+			var resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+			builder.SetContentIntent(resultPendingIntent);
+			_manager.Notify(id, builder.Build());
+		}
 
-        /// <summary>
-        /// Cancel a local notification
-        /// </summary>
-        /// <param name="id">Id of the notification to cancel</param>
-        public void Cancel(int id)
-        {
-            var intent = CreateIntent(id);
-            var pendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, intent, PendingIntentFlags.CancelCurrent);
+		public static Intent GetLauncherActivity()
+		{
+			var packageName = Application.Context.PackageName;
+			return Application.Context.PackageManager.GetLaunchIntentForPackage(packageName);
+		}
 
-            var alarmManager = GetAlarmManager();
-            alarmManager.Cancel(pendingIntent);
+		/// <summary>
+		/// Show a local notification at a specified time
+		/// </summary>
+		/// <param name="title">Title of the notification</param>
+		/// <param name="body">Body or description of the notification</param>
+		/// <param name="id">Id of the notification</param>
+		/// <param name="notifyTime">Time to show notification</param>
+		public void Show(string title, string body, int id, DateTime notifyTime)
+		{
+			var intent = CreateIntent(id);
+			var localNotification = new LocalNotification();
+			localNotification.Title = title;
+			localNotification.Body = body;
+			localNotification.Id = id;
+			localNotification.NotifyTime = notifyTime;
+			if (NotificationIconId != 0)
+			{
+				localNotification.IconId = NotificationIconId;
+			}
+			else
+			{
+				localNotification.IconId = Resource.Drawable.plugin_lc_smallicon;
+			}
 
-            var notificationManager = NotificationManagerCompat.From(Application.Context);
-            notificationManager.Cancel(id);
-        }
+			var serializedNotification = SerializeNotification(localNotification);
 
-        private Intent CreateIntent(int id)
-        {
-            return new Intent(Application.Context, typeof(ScheduledAlarmHandler))
-                .SetAction("LocalNotifierIntent" + id);
-        }
+			if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+			{
+				Java.Lang.Class javaClass = Java.Lang.Class.FromType(typeof(ScheduledJobHandler));
+				ComponentName component = new ComponentName(Application.Context, javaClass);
 
+				// Bundle up parameters
+				var extras = new PersistableBundle();
+				extras.PutString(ScheduledAlarmHandler.LocalNotificationKey, serializedNotification);
+				extras.PutInt(ScheduledJobHandler.LocalNotificationIconId, NotificationIconId);
 
-        private AlarmManager GetAlarmManager()
-        {
-            var alarmManager = Application.Context.GetSystemService(Context.AlarmService) as AlarmManager;
-            return alarmManager;
-        }
+				var triggerTime = NotifyTimeInMilliseconds(localNotification.NotifyTime) - NotifyTimeInMilliseconds(DateTime.Now);
+				JobInfo.Builder builder = new JobInfo.Builder(id, component)
+													 .SetMinimumLatency(triggerTime)   // Fire at TriggerTime
+													 .SetOverrideDeadline(triggerTime + 5000) // Or at least 5 Seconds Later
+													 .SetExtras(extras)
+													 .SetPersisted(CheckBootPermission()); //Job will be recreated after Reboot
+				JobInfo jobInfo = builder.Build();
 
-        private string SerializeNotification(LocalNotification notification)
-        {
-            var xmlSerializer = new XmlSerializer(notification.GetType());
-            using (var stringWriter = new StringWriter())
-            {
-                xmlSerializer.Serialize(stringWriter, notification);
-                return stringWriter.ToString();
-            }
-        }
+				JobScheduler jobScheduler = GetJobScheduler();
+				int result = jobScheduler.Schedule(jobInfo);
+				if (result == JobScheduler.ResultSuccess)
+				{
+					// The job was scheduled. So nothing more to do
+				}
+				else
+				{
+					// The job wasn´t scheduled. So just use the old implementation?
+					intent.PutExtra(ScheduledAlarmHandler.LocalNotificationKey, serializedNotification);
+					var pendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, intent, PendingIntentFlags.CancelCurrent);
+					var alarmManager = GetAlarmManager();
+					alarmManager.Set(AlarmType.RtcWakeup, triggerTime, pendingIntent);
+				}
+			}
+			else
+			{
+				intent.PutExtra(ScheduledAlarmHandler.LocalNotificationKey, serializedNotification);
+				var triggerTime = NotifyTimeInMilliseconds(localNotification.NotifyTime);
+				var pendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, intent, PendingIntentFlags.CancelCurrent);
+				var alarmManager = GetAlarmManager();
+				alarmManager.Set(AlarmType.RtcWakeup, triggerTime, pendingIntent);
+			}
+		}
 
-        private long NotifyTimeInMilliseconds(DateTime notifyTime)
-        {
-            var utcTime = TimeZoneInfo.ConvertTimeToUtc(notifyTime);
-            var epochDifference = (new DateTime(1970, 1, 1) - DateTime.MinValue).TotalSeconds;
+		private JobScheduler GetJobScheduler()
+		{
+			var jobScheduler = Application.Context.GetSystemService(Context.JobSchedulerService) as JobScheduler;
+			return jobScheduler;
+		}
 
-            var utcAlarmTimeInMillis = utcTime.AddSeconds(-epochDifference).Ticks / 10000;
-            return utcAlarmTimeInMillis;
-        }
-    }
+		private bool CheckBootPermission()
+		{
+			if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+			{
+				return Application.Context.CheckSelfPermission("android.permission.RECEIVE_BOOT_COMPLETED") == Android.Content.PM.Permission.Granted;
+			}
+			else
+			{
+				return Android.Support.V4.Content.PermissionChecker.CheckSelfPermission(Application.Context, "android.permission.RECEIVE_BOOT_COMPLETED") == Android.Support.V4.Content.PermissionChecker.PermissionGranted;
+			}
+		}
+
+		/// <summary>
+		/// Cancel a local notification
+		/// </summary>
+		/// <param name="id">Id of the notification to cancel</param>
+		public void Cancel(int id)
+		{
+			if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+			{
+				JobScheduler jobScheduler = GetJobScheduler();
+				jobScheduler.Cancel(id);
+			}
+			else
+			{
+				var intent = CreateIntent(id);
+				var pendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, intent, PendingIntentFlags.CancelCurrent);
+
+				var alarmManager = GetAlarmManager();
+				alarmManager.Cancel(pendingIntent);
+
+				var notificationManager = NotificationManagerCompat.From(Application.Context);
+				notificationManager.Cancel(id);
+			}
+		}
+
+		private Intent CreateIntent(int id)
+		{
+			return new Intent(Application.Context, typeof(ScheduledAlarmHandler))
+				.SetAction("LocalNotifierIntent" + id);
+		}
+
+		private AlarmManager GetAlarmManager()
+		{
+			var alarmManager = Application.Context.GetSystemService(Context.AlarmService) as AlarmManager;
+			return alarmManager;
+		}
+
+		private string SerializeNotification(LocalNotification notification)
+		{
+			var xmlSerializer = new XmlSerializer(notification.GetType());
+			using (var stringWriter = new StringWriter())
+			{
+				xmlSerializer.Serialize(stringWriter, notification);
+				return stringWriter.ToString();
+			}
+		}
+
+		private long NotifyTimeInMilliseconds(DateTime notifyTime)
+		{
+			var utcTime = TimeZoneInfo.ConvertTimeToUtc(notifyTime);
+			var epochDifference = (new DateTime(1970, 1, 1) - DateTime.MinValue).TotalSeconds;
+
+			var utcAlarmTimeInMillis = utcTime.AddSeconds(-epochDifference).Ticks / 10000;
+			return utcAlarmTimeInMillis;
+		}
+	}
 }
---
+++ src/Plugin.LocalNotifications.Android/ScheduledJobHandler.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using Android.App.Job;
+
+namespace Plugin.LocalNotifications
+{
+	[Service(Name = "plugin.localNotifications.ScheduledJobHandler", Permission = "android.permission.BIND_JOB_SERVICE")]
+	public class ScheduledJobHandler : JobService
+	{
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string LocalNotificationIconId = "LocalNotificationIconId";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jobParams"></param>
+        /// 
+        public override bool OnStartJob(JobParameters jobParams)
+        {
+            var extra = jobParams.Extras.GetString(ScheduledAlarmHandler.LocalNotificationKey);
+            var NotificationIconId = jobParams.Extras.GetInt(LocalNotificationIconId);
+            var notification = DeserializeNotification(extra);
+            LocalNotificationsImplementation.NotificationIconId = NotificationIconId;
+            CrossLocalNotifications.Current.Show(notification.Title, notification.Body, notification.Id);
+            return true;
+        }
+
+        public override bool OnStopJob(JobParameters jobParams)
+        {
+            // Called by Android when it has to terminate a running service.
+            return false; // Don't reschedule the job.
+        }
+
+        private LocalNotification DeserializeNotification(string notificationString)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(LocalNotification));
+            using (var stringReader = new StringReader(notificationString))
+            {
+                var notification = (LocalNotification)xmlSerializer.Deserialize(stringReader);
+                return notification;
+            }
+        }
+    }
+}
